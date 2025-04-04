e8c50cd0bc14e9b12acfd35a8e1fb3acbceedfc1
Peter Utomakili <fezphantom@gmail.com>
2025-04-04 14:39:57 -0230

    result analysis of top paying data science roles

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-TODO: Update the contents of this later
+ # Introduction
+ This analysis focuses on the data job market, specifically emphasizing Data Science roles. It highlights top-paying positions and identifies optimal skills that bridge the gap between in-demand expertise and high salaries. By examining where high demand intersects with top compensation, this insight helps data professionals align their skill sets with the most lucrative opportunities.
+ For SQL queries, kindly check the link to the folder
+ [project_sql folder](/project_sql/)
+ # Background
+ # Tools used
+- **SQL :** Used to extract, manipulate, and analyze job posting data. Queries were optimized to filter roles, compute demand metrics, and aggregate salary insights.
+- **PostgreSQL :** A powerful relational database system that stored and managed the job market dataset. It facilitated advanced querying, data transformations, and statistical aggregations.
+- **Visual Studio Code (VS Code) :** The primary development environment for writing and debugging SQL queries. Extensions for PostgreSQL integration streamlined database interaction.
+- **Git & GitHub :** Git was used for version control, tracking changes in SQL scripts and ensuring a structured workflow. GitHub served as a centralized repository for storing and sharing the analysis, enabling collaboration, documentation, and version history tracking.
+ # Analysis
+ # COnclusions
--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -18,3 +18,97 @@ WHERE
 ORDER BY
     salary_year_avg DESC
 LIMIT 10;
+
+/*
+🏁 Takeaway
+If you're targeting top-tier Data Science roles in Canada, aim for:
+
+- Leadership or Staff-level titles
+- Strong industry context (FinTech, EdTech, SaaS, etc.)
+- Full-stack data skills with the ability to manage both people and pipelines
+- Roles offering remote flexibility are increasingly lucrative
+
+[
+  {
+    "job_id": 377672,
+    "company_name": "Mercury",
+    "job_title": "Head of Data",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "350000.0",
+    "job_posted_date": "2023-03-15 09:16:26"
+  },
+  {
+    "job_id": 1499100,
+    "company_name": "Course Hero",
+    "job_title": "Staff Strategic Data Scientist",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "224000.0",
+    "job_posted_date": "2023-03-11 01:27:48"
+  },
+  {
+    "job_id": 1505300,
+    "company_name": "Secureframe",
+    "job_title": "Data and Analytics Lead",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-01-12 22:39:39"
+  },
+  {
+    "job_id": 1232291,
+    "company_name": "Figma",
+    "job_title": "Data Scientist, Product",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "200000.0",
+    "job_posted_date": "2023-12-03 21:05:50"
+  },
+  {
+    "job_id": 276555,
+    "company_name": "MongoDB",
+    "job_title": "Staff Analytics Engineer",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "197500.0",
+    "job_posted_date": "2023-11-28 18:16:00"
+  },
+  {
+    "job_id": 746652,
+    "company_name": "Wish",
+    "job_title": "Data Scientist",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "180000.0",
+    "job_posted_date": "2023-04-12 09:31:20"
+  },
+  {
+    "job_id": 539196,
+    "company_name": "Citigroup, Inc",
+    "job_title": "Big Data Development Lead",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "175000.0",
+    "job_posted_date": "2023-04-08 08:29:15"
+  },
+  {
+    "job_id": 13447,
+    "company_name": "Autodesk, Inc",
+    "job_title": "Lead Data Scientist, Product Insights",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "175000.0",
+    "job_posted_date": "2023-03-14 06:27:47"
+  },
+  {
+    "job_id": 288504,
+    "company_name": "Zynga",
+    "job_title": "Data Scientist (Full Stack) - Marketing",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "172000.0",
+    "job_posted_date": "2023-10-11 12:18:30"
+  },
+  {
+    "job_id": 935101,
+    "company_name": "Very",
+    "job_title": "Principal Data Scientist - Canada - Remote",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "161250.0",
+    "job_posted_date": "2023-10-02 15:42:26"
+  }
+]
+
+*/
